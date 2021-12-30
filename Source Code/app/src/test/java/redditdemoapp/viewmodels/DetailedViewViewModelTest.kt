f0be743f7f050afcba5e31b4f39797e15de2a8a1
pszumlanski <przeszumlanski@gmail.com>
2021-12-30 12:47:29 +0700

    Updated testing for DetailedViewViewModel, test passed

--- Source Code/app/src/test/java/redditdemoapp/viewmodels/DetailedViewViewModelTest.kt
+++ Source Code/app/src/test/java/redditdemoapp/viewmodels/DetailedViewViewModelTest.kt
@@ -1,53 +1,69 @@
 package redditdemoapp.viewmodels
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import redditdemoapp.models.ChildrenPostsDataGsonModel
+import redditdemoapp.models.PostGsonModel
+import redditdemoapp.models.PostsResponseGsonModel
+import redditdemoapp.models.SinglePostDataGsonModel
+import redditdemoapp.network.ApiClient
+import redditdemoapp.utils.DataFetchingCallback
+import retrofit2.mock.Calls
+
 class DetailedViewViewModelTest {
 
-//    private var viewModel: DetailedViewViewModel? = null
-//    private var fakeSingleCharacterObject: CompleteCharacterObject? = null
-//    private var fakeSingleCharacterList: List<CompleteCharacterObject>? = null
-//    private var fakeSingleCharacterId: Int? = null
-//
-//    @Mock
-//    private val apiClient: ApiClient? = null
-//
-//    @get:Rule
-//    var rule: TestRule = InstantTaskExecutorRule()
-//
-//    @Before
-//    fun setupTest() {
-//
-//        // Inject Mocks
-//        MockitoAnnotations.initMocks(this)
-//
-//        // Initialize the ViewModel
-//        viewModel = DetailedViewViewModel(apiClient!!)
-//
-//        // Prepare fake single character list
-//        fakeSingleCharacterObject = CompleteCharacterObject(0, "John Smith", "Johnny",
-//            null, listOf(3), listOf(1, 2, 3), "02.02.1990", emptyList(),
-//            "Boss", "John Travolta", "Breaking Bad")
-//        fakeSingleCharacterList = listOf(fakeSingleCharacterObject!!)
-//
-//        // Set the id of character to be used by API endpoint
-//        fakeSingleCharacterId = 0
-//    }
-//
-//    @Test
-//    fun fetchSelectedCharacterByFeedViewModel() {
-//
-//        // Prepare API response
-//        val fakeGetSelectedCharacterResponseObject = Calls.response(fakeSingleCharacterList!!)
-//
-//        // Set testing conditions
-////        Mockito.`when`(apiClient?.getSingleCharacterById(fakeSingleCharacterId!!)).thenReturn(fakeGetSelectedCharacterResponseObject)
-//
-//        // Prepare fake callback
-//        val fakeCallback: DataFetchingCallback = Mockito.mock(DataFetchingCallback::class.java)
-//
-//        // Perform the action
-////        viewModel!!.getSelectedCharacter(fakeCallback, fakeSingleCharacterId!!)
-//
-//        // Check whether correct callback method has been called
-//        Mockito.verify(fakeCallback).fetchingSuccessful(fakeSingleCharacterObject!!)
-//    }
+    private var viewModel: DetailedViewViewModel? = null
+    private var fakeSinglePostObject: SinglePostDataGsonModel? = null
+    private var fakeSinglePostList: List<PostsResponseGsonModel>? = null
+    private var fakeSinglePostId: String? = null
+
+    @Mock
+    private val apiClient: ApiClient? = null
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupTest() {
+
+        // Inject Mocks
+        MockitoAnnotations.initMocks(this)
+
+        // Initialize the ViewModel
+        viewModel = DetailedViewViewModel(apiClient!!)
+
+        // Prepare fake single character list
+        fakeSinglePostObject = SinglePostDataGsonModel(PostGsonModel("0A", "/r/Android/1", "Your Daily Thread", "Android", "image1"))
+        val fakePostList = listOf(fakeSinglePostObject!!)
+        val childrenPostsDataGsonModel = ChildrenPostsDataGsonModel(fakePostList)
+        fakeSinglePostList = listOf(PostsResponseGsonModel(childrenPostsDataGsonModel))
+
+        // Set the id of character to be used by API endpoint
+        fakeSinglePostId = "0A"
+    }
+
+    @Test
+    fun fetchSelectedCharacterByFeedViewModel() {
+
+        // Prepare API response
+        val fakeGetSelectedPostResponseObject = Calls.response(fakeSinglePostList!!)
+
+        // Set testing conditions
+        Mockito.`when`(apiClient?.getSinglePostById(fakeSinglePostId!!)).thenReturn(fakeGetSelectedPostResponseObject)
+
+        // Prepare fake callback
+        val fakeCallback: DataFetchingCallback = Mockito.mock(DataFetchingCallback::class.java)
+
+        // Perform the action
+        viewModel!!.getSelectedPost(fakeCallback, fakeSinglePostId!!)
+
+        // Check whether correct callback method has been called
+        Mockito.verify(fakeCallback).fetchingSuccessful(fakeSinglePostList!!.first().data.childrenPosts.first().post)
+    }
 }
