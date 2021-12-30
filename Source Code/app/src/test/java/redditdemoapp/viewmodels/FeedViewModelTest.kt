383d1f99422d9dde02db3e80b14ee4fe234c67a5
pszumlanski <przeszumlanski@gmail.com>
2021-12-30 12:00:07 +0700

    Fixed issues with Mockito, test for FeedViewModel now passes

--- Source Code/app/src/test/java/redditdemoapp/viewmodels/FeedViewModelTest.kt
+++ Source Code/app/src/test/java/redditdemoapp/viewmodels/FeedViewModelTest.kt
@@ -1,52 +1,72 @@
 package redditdemoapp.viewmodels
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import redditdemoapp.models.ChildrenPostsDataGsonModel
+import redditdemoapp.models.PostGsonModel
+import redditdemoapp.models.PostsResponseGsonModel
+import redditdemoapp.models.SinglePostDataGsonModel
+import redditdemoapp.network.ApiClient
+import redditdemoapp.utils.DataFetchingCallback
+import redditdemoapp.utils.FilteringTools
+import retrofit2.mock.Calls
+
 class FeedViewModelTest {
 
-//    private var viewModel: FeedViewModel? = null
-//    private var fakeCharactersList: List<SimplifiedCharacterObject>? = null
-//
-//    @Mock
-//    private val apiClient: ApiClient? = null
-//
-//    @Mock
-//    private val filteringTools: FilteringTools? = null
-//
-//    @get:Rule
-//    var rule: TestRule = InstantTaskExecutorRule()
-//
-//    @Before
-//    fun setupTest() {
-//
-//        // Inject Mocks
-//        MockitoAnnotations.initMocks(this)
-//
-//        // Initialize the ViewModel
-//        viewModel = FeedViewModel(apiClient!!, filteringTools!!)
-//
-//        // Prepare fake characters list
-//        val character1 = SimplifiedCharacterObject(0, "John Smith", "Johnny", null, listOf(3), listOf(1, 2, 3))
-//        val character2 = SimplifiedCharacterObject(1, "John Connery", "J.C.", null, listOf(1, 2), listOf(1))
-//        val character3 = SimplifiedCharacterObject(2, "Sue Connery", "S.C.", null, listOf(1, 2), listOf(1))
-//        fakeCharactersList = listOf(character1, character2, character3)
-//    }
-//
-//    @Test
-//    fun fetchAllCharactersByFeedViewModel() {
-//
-//        // Prepare API response
-//        val fakeGetCharactersResponseObject = Calls.response(fakeCharactersList!!)
-//
-//        // Set testing conditions
-////        Mockito.`when`(apiClient?.getAllCharacters()).thenReturn(fakeGetCharactersResponseObject)
-////        Mockito.`when`(filteringTools?.filterResults(fakeCharactersList!!, null, null)).thenReturn(fakeCharactersList)
-//
-//        // Prepare fake callback
-//        val fakeCallback: DataFetchingCallback = Mockito.mock(DataFetchingCallback::class.java)
-//
-//        // Perform the action
-////        viewModel!!.getCharacters(fakeCallback, null, null)
-//
-//        // Check whether correct callback method has been called
-//        Mockito.verify(fakeCallback).fetchingSuccessful(fakeCharactersList!!)
-//    }
+    private var viewModel: FeedViewModel? = null
+    private var fakePosts: PostsResponseGsonModel? = null
+
+    @Mock
+    private val apiClient: ApiClient? = null
+
+    @Mock
+    private val filteringTools: FilteringTools? = null
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupTest() {
+
+        // Inject Mocks
+        MockitoAnnotations.initMocks(this)
+
+        // Initialize the ViewModel
+        viewModel = FeedViewModel(apiClient!!, filteringTools!!)
+
+        // Prepare fake posts list
+        val post1 = SinglePostDataGsonModel(PostGsonModel("0A", "/r/Android/1", "Your Daily Thread", "Android", "image1"))
+        val post2 = SinglePostDataGsonModel(PostGsonModel("1B", "/r/Android/2", "My Daily Thread", "Studio", "image2"))
+        val post3 = SinglePostDataGsonModel(PostGsonModel("2C", "/r/Android/3", "Her Daily Thread", "Reddit", "image3"))
+        val fakePostList = listOf(post1, post2, post3)
+
+        val childrenPostsDataGsonModel = ChildrenPostsDataGsonModel(fakePostList)
+        fakePosts = PostsResponseGsonModel(childrenPostsDataGsonModel)
+    }
+
+    @Test
+    fun fetchAllCharactersByFeedViewModel() {
+
+        // Prepare API response
+        val fakeGetPostsResponseObject = Calls.response(fakePosts!!)
+
+        // Set testing conditions
+        Mockito.`when`(apiClient?.getFreshPosts()).thenReturn(fakeGetPostsResponseObject)
+        Mockito.`when`(filteringTools?.filterResults(fakePosts!!.data.childrenPosts, null)).thenReturn(fakePosts!!.data.childrenPosts)
+
+        // Prepare fake callback
+        val fakeCallback: DataFetchingCallback = Mockito.mock(DataFetchingCallback::class.java)
+
+        // Perform the action
+        viewModel!!.getPosts(fakeCallback, null)
+
+        // Check whether correct callback method has been called
+        Mockito.verify(fakeCallback).fetchingSuccessful(fakePosts!!.data.childrenPosts)
+    }
 }
